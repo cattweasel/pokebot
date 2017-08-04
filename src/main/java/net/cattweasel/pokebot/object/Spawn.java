8a51b39c901784da1e87f4847c2c5706ea1dcf26
benjamin.wesp <benjamin.wesp@172.16.40.105>
2017-08-04 13:11:06 +0200

    Data model extension

---
+++ src/main/java/net/cattweasel/pokebot/object/Spawn.java
@@ -0,0 +1,78 @@
+package net.cattweasel.pokebot.object;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import net.cattweasel.pokebot.tools.xml.DateAdapter;
+import net.cattweasel.pokebot.tools.xml.WrappedReferenceAdapter;
+
+public class Spawn extends PokeObject {
+	
+	private static final long serialVersionUID = 394496324808164843L;
+	
+	private Pokemon pokemon;
+	private Date disappearTime;
+	private Integer eid;
+	private Double latitude;
+	private Double longitude;
+	
+	@ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "pokemon", unique = false, nullable = false)
+	@XmlElement(name = "Pokemon")
+	@XmlJavaTypeAdapter(WrappedReferenceAdapter.class)
+	public Pokemon getPokemon() {
+		return pokemon;
+	}
+	
+	public void setPokemon(Pokemon pokemon) {
+		this.pokemon = pokemon;
+	}
+	
+	@Column(unique = false, nullable = false)
+	@XmlAttribute
+	@XmlJavaTypeAdapter(DateAdapter.class)
+	public Date getDisappearTime() {
+		return disappearTime;
+	}
+
+	public void setDisappearTime(Date disappearTime) {
+		this.disappearTime = disappearTime;
+	}
+
+	@Column(unique = false, nullable = false)
+	@XmlAttribute
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	@Column(unique = false, nullable = false)
+	@XmlAttribute
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	@Column(unique = false, nullable = false)
+	@XmlAttribute
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+}
