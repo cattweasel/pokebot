59bea051b35372d93ec527c3058d50a1a35ce1fb
benjamin.wesp <benjamin.wesp@192.168.108.74>
2017-08-23 10:40:30 +0200

    Adjusted some map related issues

--- src/main/java/net/cattweasel/pokebot/web/bean/MapBean.java
+++ src/main/java/net/cattweasel/pokebot/web/bean/MapBean.java
@@ -5,7 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 
 import net.cattweasel.pokebot.object.BotSession;
-import net.cattweasel.pokebot.object.Capability;
+import net.cattweasel.pokebot.object.Filter;
+import net.cattweasel.pokebot.object.Gym;
+import net.cattweasel.pokebot.object.QueryOptions;
+import net.cattweasel.pokebot.object.Spawn;
+import net.cattweasel.pokebot.object.UserNotification;
 import net.cattweasel.pokebot.tools.GeneralException;
 import net.cattweasel.pokebot.tools.Util;
 
@@ -15,6 +19,7 @@ public class MapBean extends BaseBean {
 		
 		private Double x;
 		private Double y;
+		private Integer notifications;
 		
 		public Double getX() {
 			return x;
@@ -23,35 +28,51 @@ public class MapBean extends BaseBean {
 		public void setX(Double x) {
 			this.x = x;
 		}
-		
+
 		public Double getY() {
 			return y;
 		}
-		
+
 		public void setY(Double y) {
 			this.y = y;
 		}
+
+		public Integer getNotifications() {
+			return notifications;
+		}
+
+		public void setNotifications(Integer notifications) {
+			this.notifications = notifications;
+		}
 	}
 	
+	private static final String NAME = "name";
 	private static final String LATITUDE = "latitude";
 	private static final String LONGITUDE = "longitude";
 	
 	public List<Location> getUserLocations() throws GeneralException {
 		List<Location> result = new ArrayList<Location>();
-		if (getLoggedInUser() != null && getLoggedInUser().hasCapability(Capability.SYSTEM_ADMINISTRATOR)) {
-			Iterator<String> it = getContext().search(BotSession.class);
-			if (it != null) {
-				while (it.hasNext()) {
-					BotSession session = getContext().getObjectById(BotSession.class, it.next());
-					if (session.get(LATITUDE) != null && session.get(LONGITUDE) != null) {
-						Location l = new Location();
-						l.setX(Util.atod(Util.otos(session.get(LATITUDE))));
-						l.setY(Util.atod(Util.otos(session.get(LONGITUDE))));
-						result.add(l);
-					}
+		Iterator<String> it = getContext().search(BotSession.class);
+		if (it != null) {
+			while (it.hasNext()) {
+				BotSession session = getContext().getObjectById(BotSession.class, it.next());
+				if (session.get(LATITUDE) != null && session.get(LONGITUDE) != null) {
+					Location l = new Location();
+					l.setX(Util.atod(Util.otos(session.get(LATITUDE))));
+					l.setY(Util.atod(Util.otos(session.get(LONGITUDE))));
+					l.setNotifications(calculateNotifications(session));
+					result.add(l);
 				}
 			}
 		}
 		return result;
+	}
+
+	private Integer calculateNotifications(BotSession session) throws GeneralException {
+		QueryOptions qo = new QueryOptions();
+		qo.addFilter(Filter.or(
+				Filter.like(NAME, String.format("%s:%s", session.getUser().getName(), Spawn.class.getSimpleName())),
+				Filter.like(NAME, String.format("%s:%s", session.getUser().getName(), Gym.class.getSimpleName()))));
+		return getContext().countObjects(UserNotification.class, qo);
 	}
 }
